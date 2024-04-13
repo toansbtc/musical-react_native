ad5a7a51bf36127bbdd38363d1ff602273996900
kyo.loan <kyo.loan@freetrend.com.vn>
2024-04-13 16:03:33 +0700

    run again but not download file

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-
   plugins: ['react-native-reanimated/plugin',
     ["module:react-native-dotenv", {
       "modulName": "@env",
@@ -11,21 +10,5 @@ module.exports = {
       "allowList": null,
       "verbose": false
     }],
-
-    [
-      'module-resolver',
-      {
-        root: ['./node_modules'],
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.js',
-          '.jsx',
-          '.json',
-          '.ts',
-          '.tsx',
-        ],
-      },
-    ],
   ]
 };
